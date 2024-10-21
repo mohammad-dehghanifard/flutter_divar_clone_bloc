0aaef45493871ebef578eaadd0d18bd7656fa7b8
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 14:16:06 -0700

    impl AuthRepository class

---
+++ lib/features/auth/repositories/auth_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/data_source/remote/auth_api_provider.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/requests/register_request.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/response/auth_response.dart';
+
+class AuthRepository {
+  final AuthApiProvider _apiProvider = AuthApiProvider();
+
+  Future<DataState<AuthResponse>> registerUserApiCall({required RegisterRequest request}) async {
+    try {
+      final Response response = await _apiProvider.provideRegisterApi(request: request);
+      if(response.statusCode == 200) {
+        return DataSuccess(AuthResponse.fromJson(response.data));
+      } else {
+        return DataFailed(response.data["message"]);
+      }
+    }catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده!");
+    }
+
+  }
+}
