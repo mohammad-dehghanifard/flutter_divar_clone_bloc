228bfd1ce1ae96cebd24aa4afc5873b334233379
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 11:22:55 -0700

    completed login view ui

--- lib/features/auth/presentation/views/login/login_view.dart
+++ lib/features/auth/presentation/views/login/login_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_divar_clone_bloc/core/common/constants/distances.dart';
+import 'package:flutter_divar_clone_bloc/core/common/constants/ui_colors.dart';
+import 'package:flutter_divar_clone_bloc/core/common/widgets/custom_button_widget.dart';
 import 'package:flutter_divar_clone_bloc/core/common/widgets/text_field_widget.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/presentation/widgets/auth_app_bar_widget.dart';
 import 'package:flutter_divar_clone_bloc/gen/assets.gen.dart';
@@ -11,9 +13,23 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // app bar
     const Widget appBar = AuthAppBarWidget(title: "ورود");
+    // input phone number widget
     final Widget phoneNumberTextField = TextFieldWidget(hintText: "شماره موبایل",iconPath: Assets.svgs.mobile,);
-    final Widget passWordTextField = TextFieldWidget(hintText: "رمز عبور",keyboardType: TextInputType.visiblePassword);
+    // input password widget
+    const Widget passWordTextField = TextFieldWidget(hintText: "رمز عبور",keyboardType: TextInputType.visiblePassword);
+    // login btn
+    final Widget loginBtn = CustomButtonWidget(onTap: () {}, text: "ورود");
+    // create account message
+    final Widget createAccount = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("حساب کاربری ندارید؟",style: TextStyle(fontSize: 16.sp,color: UiColors.blackTextColor,fontWeight: FontWeight.w900),),
+        TextButton(onPressed: () {}, child: Text("ثبت نام کنید!",style: TextStyle(fontSize: 16.sp,color: UiColors.primaryColor,fontWeight: FontWeight.w900)))
+      ],
+    );
+
     return  Scaffold(
       body: SafeArea(
           child: Padding(
@@ -24,7 +40,11 @@ class LoginView extends StatelessWidget {
                 SizedBox(height: 8.w),
                 phoneNumberTextField,
                 SizedBox(height: 4.w),
-                passWordTextField
+                passWordTextField,
+                SizedBox(height: 4.w),
+                loginBtn,
+                SizedBox(height: 4.w),
+                createAccount
               ],
             ),
           )
