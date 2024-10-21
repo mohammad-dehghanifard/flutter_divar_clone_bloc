e688ab83b8c60f3ec936d404d676ddd7b0533509
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 13:53:17 -0700

    impl EndPoints class

---
+++ lib/core/common/constants/endpoints.dart
@@ -0,0 +1,5 @@
+class EndPoints {
+  EndPoints._();
+
+  static const registerEndPoints = '/register';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
           Locale('fa'),
         ],
         locale: const Locale("fa"),
-        title: 'Divar',
+        title: 'Niaz',
         theme: ThemeManagement.lightTheme,
         routes: RouteManagement.routes,
       ),
