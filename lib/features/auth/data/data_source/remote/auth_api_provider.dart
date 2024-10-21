08036f12d4aca595f2f7be4564eece6c46423250
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 14:09:31 -0700

    impl provideRegisterApi function in AuthApiProvider

--- lib/features/auth/data/data_source/remote/auth_api_provider.dart
+++ lib/features/auth/data/data_source/remote/auth_api_provider.dart
@@ -1,9 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_divar_clone_bloc/core/common/constants/endpoints.dart';
 import 'package:flutter_divar_clone_bloc/core/utils/api/web_service.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/requests/register_request.dart';
 
 class AuthApiProvider {
 
   final _webservice = WebService();
 
-
+  Future<Response<dynamic>> provideRegisterApi({required RegisterRequest request}) async {
+    final response = await _webservice.postRequest(endPoint: EndPoints.registerEndPoints,data: request.sendRequest());
+    return response;
+  }
 
 }
--- lib/features/auth/data/response/auth_response.dart
+++ lib/features/auth/data/response/auth_response.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_divar_clone_bloc/core/common/data/model/user_model.dart';
 
-class LoginResponse {
+class AuthResponse {
   UserModel? user;
   String? token;
 
-  LoginResponse({this.user, this.token});
+  AuthResponse({this.user, this.token});
 
-  LoginResponse.fromJson(Map<String, dynamic> json) {
+  AuthResponse.fromJson(Map<String, dynamic> json) {
     user = json['user'] != null ? UserModel.fromJson(json['user']) : null;
     token = json['token'];
   }
