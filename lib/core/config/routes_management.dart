cca7c076833699a15db0b53374427e9eb358e2e8
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 09:07:16 -0700

    impl AuthView stl class

--- lib/core/config/routes_management.dart
+++ lib/core/config/routes_management.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/presentation/views/auth_view.dart';
 import 'package:flutter_divar_clone_bloc/features/splash/presentation/views/splash_view.dart';
 
 class RouteManagement {
   RouteManagement._();
 
   static Map<String, Widget Function(BuildContext)> routes = {
-      SplashView.routeName : (context) => const SplashView()
+      SplashView.routeName : (context) => const SplashView(),
+      AuthView.routeName : (context) => const AuthView(),
   };
 }
---
+++ lib/features/auth/presentation/views/auth_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AuthView extends StatelessWidget {
+  static String routeName = "/auth";
+  const AuthView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
