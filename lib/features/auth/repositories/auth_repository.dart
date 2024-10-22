32455bd21c7a14fe6221d9e7b40413ddc41915a3
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-21 20:33:37 -0700

    impl fetchProvinceApiCall function in AuthRepository

--- lib/features/auth/repositories/auth_repository.dart
+++ lib/features/auth/repositories/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_divar_clone_bloc/core/common/data/responses/perovince_response.dart';
 import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/data/data_source/remote/auth_api_provider.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/data/requests/register_request.dart';
@@ -18,6 +19,19 @@ class AuthRepository {
     }catch(e) {
       return const DataFailed("خطای ناشناخته ای رخ داده!");
     }
-
   }
+
+  Future<DataState<ProvinceResponse>> fetchProvinceApiCall() async {
+    try {
+      final Response response = await _apiProvider.provideProvinceApi();
+      if(response.statusCode == 200) {
+        return DataSuccess(ProvinceResponse.fromJson(response.data));
+      } else {
+        return DataFailed(response.statusMessage ?? "خطایی در دریافت اطلاعات رخ داده است!");
+      }
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده!");
+    }
+  }
+
 }
