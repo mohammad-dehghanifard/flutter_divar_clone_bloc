8db9e8bb7a2208b0e0b7a4845648585de6ec8347
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-23 09:50:50 -0700

    impl provideLoginApi in AuthApiProvider

--- lib/features/auth/data/data_source/remote/auth_api_provider.dart
+++ lib/features/auth/data/data_source/remote/auth_api_provider.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_divar_clone_bloc/core/common/constants/endpoints.dart';
 import 'package:flutter_divar_clone_bloc/core/utils/api/web_service.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/requests/login_request.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/data/requests/register_request.dart';
 
 class AuthApiProvider {
@@ -16,5 +17,11 @@ class AuthApiProvider {
     final response = await _webservice.getRequest(endPoint: EndPoints.provinceEndPoints);
     return response;
   }
+
+  Future<Response<dynamic>> provideLoginApi({required LoginRequest request}) async {
+    final response = await _webservice.postRequest(endPoint: EndPoints.loginEndPoints,data: request.sendRequest());
+    return response;
+  }
+
 
 }
