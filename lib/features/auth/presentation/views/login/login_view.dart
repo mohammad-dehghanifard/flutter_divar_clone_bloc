3475aae24124923263281b294cefcb1c39c0bb4e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-23 09:39:44 -0700

    connect login request to login view

--- lib/features/auth/presentation/views/login/login_view.dart
+++ lib/features/auth/presentation/views/login/login_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_divar_clone_bloc/core/common/constants/distances.dart';
 import 'package:flutter_divar_clone_bloc/core/common/constants/ui_colors.dart';
 import 'package:flutter_divar_clone_bloc/core/common/widgets/custom_button_widget.dart';
 import 'package:flutter_divar_clone_bloc/core/common/widgets/text_field_widget.dart';
+import 'package:flutter_divar_clone_bloc/features/auth/data/requests/login_request.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/presentation/views/register/register_page.dart';
 import 'package:flutter_divar_clone_bloc/features/auth/presentation/widgets/auth_app_bar_widget.dart';
 import 'package:flutter_divar_clone_bloc/gen/assets.gen.dart';
@@ -14,12 +15,22 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    final LoginRequest loginRequest = LoginRequest();
     // app bar
     const Widget appBar = AuthAppBarWidget(title: "ورود");
     // input phone number widget
-    final Widget phoneNumberTextField = TextFieldWidget(hintText: "شماره موبایل",iconPath: Assets.svgs.mobile,);
+    final Widget phoneNumberTextField = TextFieldWidget(
+      controller: loginRequest.mobileTextController,
+      validator: loginRequest.validateMobileNumber,
+      hintText: "شماره موبایل",
+      iconPath: Assets.svgs.mobile,);
     // input password widget
-    const Widget passWordTextField = TextFieldWidget(hintText: "رمز عبور",keyboardType: TextInputType.visiblePassword);
+    final Widget passWordTextField = TextFieldWidget(
+        controller: loginRequest.passWordTextController,
+        validator: loginRequest.validatePassWord,
+        hintText: "رمز عبور",
+        keyboardType: TextInputType.visiblePassword);
     // login btn
     final Widget loginBtn = CustomButtonWidget(onTap: () {}, text: "ورود");
     // create account message
