02593f2993572a7490f67cc2a759f7b5ebc6c4ae
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-23 09:49:12 -0700

    add isLoading in LoginLoadingButton and errorMessage in LoginError status

--- lib/features/auth/presentation/cubit/login/login_status.dart
+++ lib/features/auth/presentation/cubit/login/login_status.dart
@@ -3,8 +3,14 @@ sealed class LoginStatus {}
 
 final class LoginInitial extends LoginStatus {}
 
-final class LoginLoadingButton extends LoginStatus {}
+final class LoginLoadingButton extends LoginStatus {
+  final bool isLoading;
+  LoginLoadingButton({required this.isLoading});
+}
 
 final class LoginSuccess extends LoginStatus {}
 
-final class LoginError extends LoginStatus {}
+final class LoginError extends LoginStatus {
+  final String errorMessage;
+  LoginError({required this.errorMessage});
+}
