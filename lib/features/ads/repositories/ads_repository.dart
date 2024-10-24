05dbe45d13de9ecf3edea31b3dd1d9d5e76b050a
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-24 10:18:41 -0700

    impl AdsRepository class

---
+++ lib/features/ads/repositories/ads_repository.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
+import 'package:flutter_divar_clone_bloc/features/ads/data/data_source/remote/ads_api_provider.dart';
+import 'package:flutter_divar_clone_bloc/features/ads/data/models/ads_model.dart';
+
+class AdsRepository {
+  final AdsApiProvider _apiProvider = AdsApiProvider();
+
+  Future<DataState<List<AdsModel>>> getAndFilterAdsApiCall() async {
+    try {
+      final response = await _apiProvider.provideGetAndFilterAds();
+      if(response.statusCode == 200) {
+        final List<AdsModel> adsList = [];
+        for(var item in response.data["data"]) {
+          adsList.add(AdsModel.fromJson(item));
+        }
+        return DataSuccess(adsList);
+      } else {
+        return DataFailed(response.data["message"]);
+      }
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده با پشتیبانی تماس بگیرید!");
+    }
+  }
+}
