0093831aff940776e98eb313c7a7bd44890bb613
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-24 11:49:08 -0700

    impl AdsListItemWidget custom widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_divar_clone_bloc/core/common/constants/distances.dart';
 import 'package:flutter_divar_clone_bloc/core/common/constants/ui_colors.dart';
-import 'package:flutter_divar_clone_bloc/core/utils/image/load_image_network.dart';
 import 'package:flutter_divar_clone_bloc/core/utils/widgets/show_snack_bar_widget.dart';
 import 'package:flutter_divar_clone_bloc/features/ads/data/models/ads_model.dart';
+import 'package:flutter_divar_clone_bloc/features/ads/presentation/widgets/ads_list_item_widget.dart';
 import 'package:flutter_divar_clone_bloc/features/home/presentation/cubit/home_cubit.dart';
 import 'package:flutter_divar_clone_bloc/features/home/presentation/cubit/home_status.dart';
 import 'package:flutter_divar_clone_bloc/gen/assets.gen.dart';
-import 'package:flutter_divar_clone_bloc/gen/fonts.gen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -104,6 +102,7 @@ class _HomeViewState extends State<HomeView> {
         throw Exception("state inValid.....");
       }
     },);
+
     return SafeArea(
       child: BlocConsumer<HomeCubit, HomeState>(
         listener: (context, state) {
@@ -135,92 +134,4 @@ class _HomeViewState extends State<HomeView> {
   }
 }
 
-class AdsListItemWidget extends StatelessWidget {
-  const AdsListItemWidget({
-    super.key,
-    required this.ads,
-  });
 
-  final AdsModel ads;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(Distances.bodyMargin),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: UiColors.whiteColor,
-        boxShadow: [
-          BoxShadow(color: UiColors.blackColor.withOpacity(0.15),blurRadius: 10,offset: const Offset(0, 5))
-        ],
-        borderRadius: BorderRadius.circular(4.w)
-      ),
-      child: Row(
-        children: [
-          // ads image
-          SizedBox(
-            width: 30.w,
-            height: 30.w,
-            child: LoadNetworkImage(
-              radius: BorderRadius.only(
-                topRight: Radius.circular(4.w),
-                bottomRight: Radius.circular(4.w),
-              ),
-                imageUrl: ads.image!
-            ),
-          ),
-          SizedBox(width: 2.w),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // ads title
-                SizedBox(
-                  width: 50.w,
-                    child: Text(
-                      ads.title!,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: const TextStyle(fontSize: 18,fontWeight: FontWeight.w700,color: UiColors.blackTextColor))),
-                SizedBox(height: 2.w),
-                // ads price
-                RichText(
-                    text: TextSpan(
-                       text: ads.price,
-                      style: const TextStyle(
-                          fontFamily: FontFamily.yekanBakh,
-                          fontSize: 16,
-                          color: UiColors.blackTextColor,
-                      ),
-                      children: const [
-                        TextSpan(
-                          text: "  تومان",
-                          style: TextStyle(
-                            fontFamily: FontFamily.yekanBakh,
-                            fontSize: 16,
-                            color: UiColors.greyTextColor,
-                          ),
-                        )
-                      ]
-                    )
-                ),
-                SizedBox(height: 8.w),
-                // ads time and city
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: 2.5.w),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(ads.city!),
-                      Text(ads.createdAt!),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
