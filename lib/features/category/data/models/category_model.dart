b2956edb854a439b2682c4d6e26e8d526917d3a2
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-26 09:45:25 -0700

    impl category model

---
+++ lib/features/category/data/models/category_model.dart
@@ -0,0 +1,14 @@
+class CategoryModel {
+  int? id;
+  String? name;
+  String? image;
+
+  CategoryModel({this.id, this.name, this.image});
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
+  }
+
+}
