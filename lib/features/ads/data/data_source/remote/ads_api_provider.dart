b834384ae74d41bb3c36c80115ec4f8d20b4542e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-28 09:20:16 -0700

    add queryPrams in provideGetAndFilterAdsApi function

--- lib/features/ads/data/data_source/remote/ads_api_provider.dart
+++ lib/features/ads/data/data_source/remote/ads_api_provider.dart
@@ -1,12 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_divar_clone_bloc/core/common/constants/endpoints.dart';
 import 'package:flutter_divar_clone_bloc/core/utils/api/web_service.dart';
+import 'package:flutter_divar_clone_bloc/features/ads/data/params/ads_params.dart';
 
 class AdsApiProvider {
   final WebService _webService = WebService();
 
-  Future<Response<dynamic>> provideGetAndFilterAdsApi() async {
-    final response = await _webService.getRequest(endPoint: EndPoints.getAndFilterAdsEndPoints);
+  Future<Response<dynamic>> provideGetAndFilterAdsApi({AdsParams? params}) async {
+    final response = await _webService.getRequest(
+        endPoint: EndPoints.getAndFilterAdsEndPoints,
+        params: {
+          "city_id" : params?.cityId,
+          "province_id" : params?.provinceId,
+          "category_id" : params?.categoryId,
+          "order_by" : params?.orderBy,
+          "order_type" : params?.orderType,
+          "keyword" : params?.searchKeyWord,
+        }
+    );
     return response;
   }
 
