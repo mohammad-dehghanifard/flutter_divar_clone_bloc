ae2e53d017e0bbc6912ec8bad2931e162a71056a
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-28 09:13:57 -0700

    impl AdsParams class

---
+++ lib/features/ads/data/params/ads_params.dart
@@ -0,0 +1,12 @@
+class AdsParams {
+  AdsParams({
+    this.cityId,
+    this.provinceId,
+    this.categoryId,
+    this.searchKeyWord,
+    this.orderBy,
+    this.orderType,
+});
+  int? cityId,provinceId,categoryId;
+  String? searchKeyWord,orderBy,orderType;
+}
