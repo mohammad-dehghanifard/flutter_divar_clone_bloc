073745c79a59c70c71fb8ada51d5d7173622e4e6
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-30 09:55:40 -0700

    impl editUserApiCall function in  ProfileRepository

--- lib/features/profile/repositories/profile_repository.dart
+++ lib/features/profile/repositories/profile_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_divar_clone_bloc/core/common/data/model/user_model.dart';
 import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
 import 'package:flutter_divar_clone_bloc/features/profile/data/data_source/remote/profile_api_provider.dart';
+import 'package:flutter_divar_clone_bloc/features/profile/data/requests/edit_user_request.dart';
 
 class ProfileRepository {
   final ProfileApiProvider _apiProvider = ProfileApiProvider();
@@ -15,6 +16,20 @@ class ProfileRepository {
       }
     } catch(e) {
       return const DataFailed("خطای ناشناخته ای رخ داده لطفا با پشتیبانی تماس بگیرید");
+    }
+  }
+
+  Future<DataState<bool>> editUserApiCall({required EditUserRequest request}) async {
+    try {
+      final response = await _apiProvider.provideEditUserApi(request: request);
+      if(response.statusCode == 200) {
+        return const DataSuccess(true);
+      } else {
+        return DataFailed(response.data['message']);
+      }
+
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده لطفا با پشتیبانی تماس بگیرید!");
     }
   }
 }
