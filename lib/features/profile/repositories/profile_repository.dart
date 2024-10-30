6ea80da597eac0c3ce85ef65a52ecce99d1f2c60
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-30 10:11:33 -0700

    impl fetchProvinceApiCall function in ProfileRepository

--- lib/features/profile/repositories/profile_repository.dart
+++ lib/features/profile/repositories/profile_repository.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_divar_clone_bloc/core/common/data/model/user_model.dart';
+import 'package:flutter_divar_clone_bloc/core/common/data/responses/province_response.dart';
 import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
 import 'package:flutter_divar_clone_bloc/features/profile/data/data_source/remote/profile_api_provider.dart';
 import 'package:flutter_divar_clone_bloc/features/profile/data/requests/edit_user_request.dart';
@@ -30,6 +32,19 @@ class ProfileRepository {
 
     } catch(e) {
       return const DataFailed("خطای ناشناخته ای رخ داده لطفا با پشتیبانی تماس بگیرید!");
+    }
+  }
+
+  Future<DataState<ProvinceResponse>> fetchProvinceApiCall() async {
+    try {
+      final Response response = await _apiProvider.provideProvinceApi();
+      if(response.statusCode == 200) {
+        return DataSuccess(ProvinceResponse.fromJson(response.data));
+      } else {
+        return DataFailed(response.statusMessage ?? "خطایی در دریافت اطلاعات رخ داده است!");
+      }
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده!");
     }
   }
 }
