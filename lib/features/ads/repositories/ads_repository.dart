afc7b6d2870615cae6a0cd22ffa06466e7b96a2e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-11-02 10:26:54 -0700

    impl getAllCategoriesApiCall and fetchProvinceApiCall function in AdsRepository

--- lib/features/ads/repositories/ads_repository.dart
+++ lib/features/ads/repositories/ads_repository.dart
@@ -1,8 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_divar_clone_bloc/core/common/data/responses/province_response.dart';
 import 'package:flutter_divar_clone_bloc/core/common/resources/data_state.dart';
 import 'package:flutter_divar_clone_bloc/features/ads/data/data_source/remote/ads_api_provider.dart';
 import 'package:flutter_divar_clone_bloc/features/ads/data/models/ads_model.dart';
 import 'package:flutter_divar_clone_bloc/features/ads/data/models/detail_ads_model.dart';
 import 'package:flutter_divar_clone_bloc/features/ads/data/params/ads_params.dart';
+import 'package:flutter_divar_clone_bloc/features/category/data/models/category_model.dart';
 
 class AdsRepository {
   final AdsApiProvider _apiProvider = AdsApiProvider();
@@ -34,6 +37,36 @@ class AdsRepository {
       }
     } catch(e) {
       return const DataFailed("خطای ناشناخته ای رخ داده با پشتیبانی تماس بگیرید!");
+    }
+  }
+
+  Future<DataState<List<CategoryModel>>> getAllCategoriesApiCall() async {
+    try {
+      final response = await _apiProvider.provideGetAllCategoryApi();
+      if(response.statusCode == 200) {
+        final List<CategoryModel> categories = [];
+        for(Map<String,dynamic> category in response.data["data"]) {
+          categories.add(CategoryModel.fromJson(category));
+        }
+        return DataSuccess(categories);
+      } else {
+        return DataFailed(response.data["message"]);
+      }
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده لطفا با پشتیبانی تماس بگیرید!");
+    }
+  }
+
+  Future<DataState<ProvinceResponse>> fetchProvinceApiCall() async {
+    try {
+      final Response response = await _apiProvider.provideProvinceApi();
+      if(response.statusCode == 200) {
+        return DataSuccess(ProvinceResponse.fromJson(response.data));
+      } else {
+        return DataFailed(response.statusMessage ?? "خطایی در دریافت اطلاعات رخ داده است!");
+      }
+    } catch(e) {
+      return const DataFailed("خطای ناشناخته ای رخ داده!");
     }
   }
 
